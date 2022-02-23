afcbdb275470128f787228e0a96b7b5f06acbd4c
FadhelEssidEsprit <fadhel.essid@esprit.tn>
2022-02-23 19:30:33 +0100

    Create Category.java

---
+++ src/entities/Category.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author fadhe
+ */
+public class Category {
+    
+    private String category;
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.category);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (!Objects.equals(this.category, other.category)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "category=" + category + '}';
+    }
+    
+    
+    
+}
