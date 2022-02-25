67b735796b6c29e4ffe58171ae1030759c337319
Khaled Mihoub <49345013+khaledmihoub@users.noreply.github.com>
2022-02-25 23:33:25 +0100

    update3

--- src/GUI/Home_InterfaceController.java
+++ src/GUI/Home_InterfaceController.java
@@ -5,9 +5,15 @@
  */
 package GUI;
 
+import entities.Chat;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import services.ChatService;
 
 /**
  * FXML Controller class
@@ -15,13 +21,30 @@ import javafx.fxml.Initializable;
  * @author nadee ben cheikh
  */
 public class Home_InterfaceController implements Initializable {
+    
+   
+    @FXML
+    private Button btn_send_message;
+    @FXML
+    private TextField textfield_message;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        
     }    
+
+    @FXML
+    private void send_message(ActionEvent event) {
+        String message = textfield_message.getText();
+        Chat m1 = new Chat(1,message); 
+        ChatService cs = new ChatService();
+        cs.SendFile(m1);
+       
+       
+    }
     
 }
